e49854175d4fbc5492a1e0938e75e24ec3f8a621
doliveira <doliveira@tripadvisor.com>
2024-03-22 22:36:39 -0300

    changes till the chapter 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { BeVietnamPro } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${BeVietnamPro.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Be_Vietnam_Pro, Lusitana } from 'next/font/google';
+ 
+export const BeVietnamPro = Be_Vietnam_Pro({ weight: ['200', '400'], subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] });
